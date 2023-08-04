b244d74d7f22854cac97015754435f4f1d0a366e
Patricio_Bordon <patriciobordon123@gmail.com>
2023-08-03 23:19:09 -0300

    Variable ID constante

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -5,18 +5,18 @@ public class Empleado {
 	private String Nombre;
 	private int Edad;
 	
-	private int id;
-	static int cont=999;
+	private final int id;
+	static int cont=999; 
 	
 	public Empleado(){
-		 cont++;
-		 this.id= cont;
+		 cont++; 
+		 this.id= cont; 
 		 this.Nombre="sin nombre";
 		 this.Edad=99;
 	 }
 	public Empleado(String nombre,int edad){
-		 cont++;
-		 this.id= cont;
+		 cont++; 
+		 this.id= cont; 
 		 this.Nombre=nombre;
 		 this.Edad=edad;
 	 }
