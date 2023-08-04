c61bb071575c3c58dea15ebcce89560a6863339d
Patricio_Bordon <patriciobordon123@gmail.com>
2023-08-03 23:26:14 -0300

    Correcion variable ID repetida

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,22 +1,21 @@
 package ejercicio1;
 
 public class Empleado {
-	private int Id;
 	private String Nombre;
 	private int Edad;
 	
-	private final int id;
-	static int cont=999; 
+	private final int id;  
+	static int cont=999;  
 	
 	public Empleado(){
-		 cont++; 
-		 this.id= cont; 
+		 cont++;   
+		 this.id= cont;  
 		 this.Nombre="sin nombre";
 		 this.Edad=99;
 	 }
 	public Empleado(String nombre,int edad){
-		 cont++; 
-		 this.id= cont; 
+		 cont++;  
+		 this.id= cont;  
 		 this.Nombre=nombre;
 		 this.Edad=edad;
 	 }
