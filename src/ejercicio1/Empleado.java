2ed35c2618acefd7c829a2c5a75048fccf152770
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-07 12:42:52 -0300

    Aplicar Google Java Style Guide
    https://google.github.io/styleguide/javaguide.html

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,34 +1,32 @@
 package ejercicio1;
 
 public class Empleado {
-	/* Properties */
-	private final int id;
+
+	private final int ID;
 	private String nombre;
 	private int edad;
-	/* Static properties */
-	private static int idCounter = 999;
 	
-	/* Static methods */
+	static int idCounter = 999;
+	
 	public static int getNextID() {
 		return idCounter + 1;
 	}
-	/* Constructors */
+
 	public Empleado() {
 		idCounter++;
-		this.id = idCounter;
+		this.ID = idCounter;
 		this.nombre = "Unknown";
 		this.edad = 0;
 	}
 	public Empleado(String nombre, int edad) {
 		idCounter++;
-		this.id = idCounter;
+		this.ID = idCounter;
 		this.nombre = nombre;
 		this.edad = edad;
 	}
-	
-	/* Getters */
+
 	public int getId() {
-		return id;
+		return this.ID;
 	}
 	public String getNombre() {
 		return nombre;
@@ -36,14 +34,14 @@ public class Empleado {
 	public int getEdad() {
 		return edad;
 	}
-	/* Setters */
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	/* Other methods */
+
 	@Override
 	public String toString() {
 		return "Empleado { id: " + getId() + ", nombre: " + getNombre() + ", edad: " + getEdad() + " }";
--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -3,15 +3,15 @@ package ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		Empleado[] empleados = new Empleado[5];
-		empleados[0] = new Empleado("Máximo Canedo", 20);
-		empleados[1] = new Empleado("Adrián Suárez", 40);
-		empleados[2] = new Empleado();
-		empleados[3] = new Empleado("Hector da Silva", 40);
-		empleados[4] = new Empleado("Inés Gregoria", 50);
-		
-		for(int i = 0; i < 5; i++) {
-			System.out.println(empleados[i].toString());
+		Empleado[] empleados = new Empleado[] {
+				new Empleado("Máximo Canedo", 20),
+				new Empleado("Adrián Suárez", 40),
+				new Empleado(),
+				new Empleado("Hector da Silva", 40),
+				new Empleado("Inés Gregoria", 50)
+		};
+		for(Empleado e: empleados) {
+			System.out.println(e.toString());
 		}
 		System.out.println("El siguiente empleado tendrá el ID #" + Empleado.getNextID() + ". ");
 	}
