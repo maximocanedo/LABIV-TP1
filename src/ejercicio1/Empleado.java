518fa13d370cd79d0adaff226a219fad7ceee065
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-07 12:02:47 -0300

    Encapsular propiedades #2

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,7 +1,32 @@
 package ejercicio1;
 
 public class Empleado {
-	int id;
-	String nombre;
-	int edad;
+	/* Properties */
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	
+	
+	/* Getters */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	/* Setters */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 }
