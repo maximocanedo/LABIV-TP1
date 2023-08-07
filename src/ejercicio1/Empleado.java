4e237c0fb588925fe25cbc8c26a5b023f211e811
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-07 12:23:50 -0300

    Resolución de los otros puntos

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -2,31 +2,51 @@ package ejercicio1;
 
 public class Empleado {
 	/* Properties */
-	private int id;
+	private final int id;
 	private String nombre;
 	private int edad;
+	/* Static properties */
+	private static int idCounter = 999;
 	
-	
+	/* Static methods */
+	public static int getNextID() {
+		return idCounter + 1;
+	}
+	/* Constructors */
+	public Empleado() {
+		idCounter++;
+		this.id = idCounter;
+		this.nombre = "Unknown";
+		this.edad = 0;
+	}
+	public Empleado(String nombre, int edad) {
+		idCounter++;
+		this.id = idCounter;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
 	
 	/* Getters */
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
 	public String getNombre() {
 		return nombre;
+	}
+	public int getEdad() {
+		return edad;
 	}
 	/* Setters */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public int getEdad() {
-		return edad;
-	}
 	public void setEdad(int edad) {
 		this.edad = edad;
+	}
+	/* Other methods */
+	@Override
+	public String toString() {
+		return "Empleado { id: " + getId() + ", nombre: " + getNombre() + ", edad: " + getEdad() + " }";
 	}
 	
 }
